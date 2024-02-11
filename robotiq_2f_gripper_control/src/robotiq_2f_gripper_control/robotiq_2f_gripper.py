64971738e573f21602e2d8e6f1bf424e7aaae6b1
maribellae <71653479+maribellae@users.noreply.github.com>
2024-02-11 11:48:52 +0300

    Update robotiq_2f_gripper.py

--- robotiq_2f_gripper_control/src/robotiq_2f_gripper_control/robotiq_2f_gripper.py
+++ robotiq_2f_gripper_control/src/robotiq_2f_gripper_control/robotiq_2f_gripper.py
@@ -2,6 +2,7 @@ import serial
 from serial.serialutil import SerialException
 
 from pymodbus.client import ModbusSerialClient
+import robotiq_modbus_rtu
 import robotiq_modbus_rtu.comModbusRtu
 
 from math import ceil

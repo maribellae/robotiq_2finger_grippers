df05cc3d2db3cc4d48205ac3e5aab23663c6810a
maribellae <71653479+maribellae@users.noreply.github.com>
2024-02-15 21:43:13 +0300

    Update gripper.py

--- robotiq_2f_gripper_control/scripts/gripper.py
+++ robotiq_2f_gripper_control/scripts/gripper.py
@@ -2,12 +2,12 @@
 import sys
 import time
 import rospy
-from robotiq_2f_gripper_msgs.msg import RobotiqGripperCommand
-from robotiq_2f_gripper_msgs.action import CommandRobotiqGripper
-from robotiq_2f_gripper_control.robotiq_2f_gripper_driver import Robotiq2FingerGripperDriver as Robotiq
-import actionlib
 from robotiq_2f_gripper_msgs.msg import CommandRobotiqGripperFeedback, CommandRobotiqGripperResult, CommandRobotiqGripperAction, CommandRobotiqGripperGoal
 from robotiq_2f_gripper_control.robotiq_2f_gripper_driver import Robotiq2FingerGripperDriver as Robotiq
+#from robotiq_2f_gripper_control.robotiq_2f_gripper_driver import Robotiq2FingerGripperDriver as Robotiq
+import actionlib
+#from robotiq_2f_gripper_msgs.msg import CommandRobotiqGripperFeedback, CommandRobotiqGripperResult, CommandRobotiqGripperAction, CommandRobotiqGripperGoal
+#from robotiq_2f_gripper_control.robotiq_2f_gripper_driver import Robotiq2FingerGripperDriver as Robotiq
 
 
 class GripperPosControl:
@@ -15,7 +15,7 @@ class GripperPosControl:
       
         rospy.init_node("gripper") #ur3_joints
         action_name = rospy.get_param('~action_name', 'command_robotiq_action')
-        self.robotiq_client = actionlib.SimpleActionClient(action_name, CommandRobotiqGripper) #CommandRobotiqGripperAction
+        self.robotiq_client = actionlib.SimpleActionClient(action_name, CommandRobotiqGripperAction) #CommandRobotiqGripperAction
       
         # Wait until grippers are ready to take command
         robotiq_client.wait_for_server()
@@ -24,7 +24,7 @@ class GripperPosControl:
         ## ROS Subscriber Topic
         # Subscribe to the ar_pose_marker topic to get the image width and height
 
-        self.ur_sub_gripper = rospy.Subscriber('gripper',RobotiqGripperCommand, self.callback_gripper) #CommandRobotiqGripperGoal
+        self.ur_sub_gripper = rospy.Subscriber('gripper',CommandRobotiqGripperAction, self.callback_gripper) #CommandRobotiqGripperGoal
         
         ## ROS parameters
         self.rate = rospy.Rate(1) # Hz
